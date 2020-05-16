540bad35a4cacea52852ea3c84c85e527fe3545e
Jeongsu Yang <mycat83@gmail.com>
2020-05-16 22:41:19 +0900

    Initial commit

---
+++ src/main/java/me/study/cloud/StudySpringCloudApplication.java
@@ -0,0 +1,13 @@
+package me.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/study/cloud/StudySpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
