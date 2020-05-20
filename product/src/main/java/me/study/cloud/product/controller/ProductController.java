8708423c0a61cc4fea65da25c58098d3c07c297e
Jeongsu Yang <mycat83@gmail.com>
2020-05-20 23:38:02 +0900

    Hystrix - Circuit Breaker 적용

--- product/src/main/java/me/study/cloud/product/controller/ProductController.java
+++ product/src/main/java/me/study/cloud/product/controller/ProductController.java
@@ -10,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @GetMapping("/{productId}")
-    public String getProduct(@PathVariable String productId) {
+    public String getProductInfo(@PathVariable String productId) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        throw new RuntimeException("I/O Exception");
+        System.out.println("Called product id  = " + productId);
         return "[product id = " + productId + " at " + System.currentTimeMillis() + "]";
     }
 
